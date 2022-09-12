284b56e47ee4e2319b28860dccdbc02536b6051f
Sebastian Carrillo <sdcarrillo@uninorte.edu.co>
2022-09-12 16:35:12 -0500

    Getters metodos y THIS

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,16 +1,17 @@
-// export class Pokemon {
-//   public id: number;
-//   public name: string;
-
-//   constructor(id: number, name: string) {
-//     this.id = id;
-//     this.name = name;
-
-//     console.log("Constructor llamado");
-//   }
-// }
-
 export class Pokemon {
+  get imageUrl(): string {
+    return `http://pokemon.com/${this.id}.jpg`;
+  }
+
+  public scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+    this.speak();
+  }
+
+  private speak() {
+    console.log(`${this.name}, ${this.name}`);
+  }
+
   constructor(public readonly id: number, public name: string) {
     this.id = id;
     this.name = name;
@@ -20,3 +21,6 @@ export class Pokemon {
 }
 
 export const charmander = new Pokemon(4, "Charmander");
+console.log("🚀 ~ file: 03-classes.ts ~ line 27 ~ charmander", charmander);
+
+charmander.scream();
